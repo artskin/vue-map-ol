e989a08d180e9afbc78042660b4e2c2720650e4c
Gu Liqiang <guliqiang@domain.sensetime.com>
2018-09-04 14:42:13 +0800

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,22 @@
 
 <template>
   <div>
-  <h1>home</h1>
+  <h1>{{msg}}</h1>
   
   </div>
 </template>
 
 <script>  
-  
+
+  export default {
+
+    data () {
+      return {
+        msg: 'Testing 2 vue2MapOlSwiss21781 Maps side by side',
+      }
+    },
+    methods: {
+      
+    }
+  }
 </script>

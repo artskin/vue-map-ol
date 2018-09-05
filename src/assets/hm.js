ed2989a3311d575d8c85e9d1d1d5f8a0d1f62ba1
lan <iorilan@hotmail.com>
2018-09-05 10:47:41 +0800

    heat map js and dummy data file

--- src/assets/hm.js
+++ src/assets/hm.js
@@ -9,29 +9,51 @@
       //// url to fetch geojson data
       var urlData = "https://raw.githubusercontent.com/artskin/vue-map-ol/master/src/assets/data/hm-test.json";
 
-      //// TODO may need to calculate based on the # of ppl
+      //// const
       var blurV = 50;
       var radiusV = 35;
-      //// refresh data
+      var frequencyInSecond = 10 * 1000;
 
-
+	console.log("hm 1");
       var vectorSource = new VectorSource({
         url: urlData,
         format: new GeoJSON(),
         wrapX: false
         //features: (new GeoJSON()).readFeatures(dummyobj)
       });
+console.log("hm 2");
       var vector = new HeatmapLayer({
         source: vectorSource,
         blur: blurV,
         radius: radiusV
       });
+	  console.log("hm 3");
+      ////realtime refresh
+      var req = require('request');
+       var realtimeRefreshData = function(){
+           setTimeout(() => {
+             req(urlData, function (error, response, body) {
+               console.log("response body:");
+               console.log(body);
+               console.log("vector :");
+               console.log(vectorSource);
+
+              
+             realtimeRefreshData();
+             });
+             console.log("refresing");
+           }, frequencyInSecond);
+       }
+      realtimeRefreshData();
+      
 
       var raster = new TileLayer({
         source: new Stamen({
           layer: 'toner'
         })
       });
+console.log("hm 4");
+      
 
       export default{
         layers:[raster, vector],

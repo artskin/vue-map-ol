04b50eafd2b7a623e6ec978cefbb70f95d278222
guliqiang <guliqiang@sensetime.com>
2018-11-14 19:09:42 +0800

    updatr

--- src/views/map_leaflet.vue
+++ src/views/map_leaflet.vue
@@ -1,331 +1,7 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 @import "../assets/style/leaflet.css";
-/* ================================================================== */
-/* Toolbars
-/* ================================================================== */
-
-.leaflet-draw-section {
-	position: relative;
-}
-
-.leaflet-draw-toolbar {
-	margin-top: 12px;
-}
-
-.leaflet-draw-toolbar-top {
-	margin-top: 0;
-}
-
-.leaflet-draw-toolbar-notop a:first-child {
-	border-top-right-radius: 0;
-}
-
-.leaflet-draw-toolbar-nobottom a:last-child {
-	border-bottom-right-radius: 0;
-}
-
-.leaflet-draw-toolbar a {
-	background-image: url('/images/spritesheet.png');
-	background-image: linear-gradient(transparent, transparent), url('/images/spritesheet.svg');
-	background-repeat: no-repeat;
-	background-size: 300px 30px;
-	background-clip: padding-box;
-}
-
-.leaflet-retina .leaflet-draw-toolbar a {
-	background-image: url('/images/spritesheet-2x.png');
-	background-image: linear-gradient(transparent, transparent), url('/images/spritesheet.svg');
-}
-
-.leaflet-draw a {
-	display: block;
-	text-align: center;
-	text-decoration: none;
-}
-
-.leaflet-draw a .sr-only {
-	position: absolute;
-	width: 1px;
-	height: 1px;
-	padding: 0;
-	margin: -1px;
-	overflow: hidden;
-	clip: rect(0, 0, 0, 0);
-	border: 0;
-}
-
-/* ================================================================== */
-/* Toolbar actions menu
-/* ================================================================== */
-
-.leaflet-draw-actions {
-	display: none;
-	list-style: none;
-	margin: 0;
-	padding: 0;
-	position: absolute;
-	left: 26px; /* leaflet-draw-toolbar.left + leaflet-draw-toolbar.width */
-	top: 0;
-	white-space: nowrap;
-}
-
-.leaflet-touch .leaflet-draw-actions {
-	left: 32px;
-}
-
-.leaflet-right .leaflet-draw-actions {
-	right: 26px;
-	left: auto;
-}
-
-.leaflet-touch .leaflet-right .leaflet-draw-actions {
-	right: 32px;
-	left: auto;
-}
-
-.leaflet-draw-actions li {
-	display: inline-block;
-}
-
-.leaflet-draw-actions li:first-child a {
-	border-left: none;
-}
-
-.leaflet-draw-actions li:last-child a {
-	-webkit-border-radius: 0 4px 4px 0;
-	border-radius: 0 4px 4px 0;
-}
-
-.leaflet-right .leaflet-draw-actions li:last-child a {
-	-webkit-border-radius: 0;
-	border-radius: 0;
-}
-
-.leaflet-right .leaflet-draw-actions li:first-child a {
-	-webkit-border-radius: 4px 0 0 4px;
-	border-radius: 4px 0 0 4px;
-}
-
-.leaflet-draw-actions a {
-	background-color: rgba(112,166,155,.6);
-	border-left: 1px solid #AAA;
-	color: #FFF;
-	font: 11px/19px "Helvetica Neue", Arial, Helvetica, sans-serif;
-	line-height: 28px;
-	text-decoration: none;
-	padding-left: 10px;
-	padding-right: 10px;
-	height: 28px;
-}
-
-.leaflet-touch .leaflet-draw-actions a {
-	font-size: 12px;
-	line-height: 30px;
-	height: 30px;
-}
-
-.leaflet-draw-actions-bottom {
-	margin-top: 0;
-}
-
-.leaflet-draw-actions-top {
-	margin-top: 1px;
-}
-
-.leaflet-draw-actions-top a,
-.leaflet-draw-actions-bottom a {
-	height: 27px;
-	line-height: 27px;
-}
-
-.leaflet-draw-actions a:hover {
-	background-color: rgba(112,166,155,1);
-}
-
-.leaflet-draw-actions-top.leaflet-draw-actions-bottom a {
-	height: 26px;
-	line-height: 26px;
-}
-
-/* ================================================================== */
-/* Draw toolbar
-/* ================================================================== */
-
-.leaflet-draw-toolbar .leaflet-draw-draw-polyline {
-	background-position: -2px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-polyline {
-	background-position: 0 -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-draw-polygon {
-	background-position: -31px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-polygon {
-	background-position: -29px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-draw-rectangle {
-	background-position: -62px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-rectangle {
-	background-position: -60px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-draw-circle {
-	background-position: -92px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-circle {
-	background-position: -90px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-draw-marker {
-	background-position: -122px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-marker {
-	background-position: -120px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-draw-circlemarker {
-	background-position: -273px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-draw-circlemarker {
-	background-position: -271px -1px;
-}
-
-/* ================================================================== */
-/* Edit toolbar
-/* ================================================================== */
-
-.leaflet-draw-toolbar .leaflet-draw-edit-edit {
-	background-position: -152px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-edit-edit {
-	background-position: -150px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-edit-remove {
-	background-position: -182px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-edit-remove {
-	background-position: -180px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-edit-edit.leaflet-disabled {
-	background-position: -212px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-edit-edit.leaflet-disabled {
-	background-position: -210px -1px;
-}
-
-.leaflet-draw-toolbar .leaflet-draw-edit-remove.leaflet-disabled {
-	background-position: -242px -2px;
-}
-
-.leaflet-touch .leaflet-draw-toolbar .leaflet-draw-edit-remove.leaflet-disabled {
-	background-position: -240px -2px;
-}
-
-/* ================================================================== */
-/* Drawing styles
-/* ================================================================== */
-
-.leaflet-mouse-marker {
-	background-color: #fff;
-	cursor: crosshair;
-}
-
-.leaflet-draw-tooltip {
-	background: rgb(54, 54, 54);
-	background: rgba(0, 0, 0, 0.5);
-	border: 1px solid transparent;
-	-webkit-border-radius: 4px;
-	border-radius: 4px;
-	color: #fff;
-	font: 12px/18px "Helvetica Neue", Arial, Helvetica, sans-serif;
-	margin-left: 20px;
-	margin-top: -21px;
-	padding: 4px 8px;
-	position: absolute;
-	visibility: hidden;
-	white-space: nowrap;
-	z-index: 6;
-}
-
-.leaflet-draw-tooltip:before {
-	border-right: 6px solid black;
-	border-right-color: rgba(0, 0, 0, 0.5);
-	border-top: 6px solid transparent;
-	border-bottom: 6px solid transparent;
-	content: "";
-	position: absolute;
-	top: 7px;
-	left: -7px;
-}
-
-.leaflet-error-draw-tooltip {
-	background-color: #F2DEDE;
-	border: 1px solid #E6B6BD;
-	color: #B94A48;
-}
-
-.leaflet-error-draw-tooltip:before {
-	border-right-color: #E6B6BD;
-}
-
-.leaflet-draw-tooltip-single {
-	margin-top: -12px
-}
-
-.leaflet-draw-tooltip-subtext {
-	color: #f8d5e4;
-}
-
-.leaflet-draw-guide-dash {
-	font-size: 1%;
-	opacity: 0.6;
-	position: absolute;
-	width: 5px;
-	height: 5px;
-}
-
-/* ================================================================== */
-/* Edit styles
-/* ================================================================== */
-
-.leaflet-edit-marker-selected {
-	background-color: rgba(254, 87, 161, 0.1);
-	border: 4px dashed rgba(254, 87, 161, 0.6);
-	-webkit-border-radius: 4px;
-	border-radius: 4px;
-	box-sizing: content-box;
-}
-
-.leaflet-edit-move {
-	cursor: move;
-}
-
-.leaflet-edit-resize {
-	cursor: pointer;
-}
-
-/* ================================================================== */
-/* Old IE styles
-/* ================================================================== */
-
-.leaflet-oldie .leaflet-draw-toolbar {
-	border: 1px solid #999;
-}
+@import "../assets/style/leaflet.draw.css";
 #map{border: 1px dashed rgba(6,77,41,.5);height: 600px;}
 .leaflet-div-icon{border-radius: 10px;transform: scale(0.3);background:rgba(255,204,0,.6);border:1px dotted rgba(255,204,0,.8);}
 </style>
@@ -334,12 +10,12 @@
   <div class="hello">
     <h1>{{ msg }}-{{id}}</h1>
     <div id="map">
-      <l-map
-      ref="map"
+      <l-map ref="map" style="height: 100%; width: 100%;"
       :min-zoom="minZoom"
+      :max-zoom="maxZoom"
       :zoom="zoom"
-      :crs="crs"
-      style="height: 100%; width: 100%;">
+      :center="center"
+      :crs="crs">
       <l-image-overlay
         :url="url"
         :bounds="bounds"/>
@@ -357,14 +33,18 @@
 
 <script>
 import { LMap, LImageOverlay, LMarker, LPopup,LPolyline } from 'vue2-leaflet';
- import { AntPath, antPath } from 'leaflet-ant-path';
-//import 'leaflet-draw';
+//轨迹模块
+import { AntPath, antPath } from 'leaflet-ant-path';
+//h绘图模块
+import 'leaflet-draw';//require('leaflet-draw');
+//热力图模块
 import 'leaflet.heat';
-require('leaflet-draw');
+
 import axios from 'axios';
 var store = require('store');
-import DrawLocal from '../assets/drawLocal.js';
 
+//项目配置
+import DrawLocal from '../assets/drawLocal.js';
 var data_geoJson = '/mock/pxmap_0F.json';
 
 export default {
@@ -376,10 +56,11 @@ export default {
       id: +new Date(),
       map: null,
       url: '/images/PX-1F.png',
-      bounds: [[-540, -960], [1080, 1920]],
+      bounds: [[-540,-960], [1080, 1920]],
       minZoom: -2,
-      maxZoom: 2,
-      zoom:-1,
+      maxZoom: 1,
+      // center: [540,960],
+      zoom:-2,
       crs: L.CRS.Simple,
       stars: [
         { name: 'Sol', lng: 175.2, lat: 145.0 },
@@ -387,13 +68,15 @@ export default {
         { name: 'Krueger-Z', lng: 13.4, lat: 56.5 },
         { name: 'Deneb', lng: 218.7, lat: 8.3 }
       ],
-      travel: [[145.0, 175.2], [8.3, 218.7]]
+      travel: []
+      //travel: [[145.0, 175.2], [8.3, 218.7]]
     }
   },
   mounted(){
     var _this = this;
     this.map = this.$refs.map.mapObject;
     var drawnItems = new L.FeatureGroup();
+    console.log(this.map.getCenter())
     
     this.map.addLayer(drawnItems);
 
@@ -402,7 +85,7 @@ export default {
         shadowUrl: null,
         iconAnchor: new L.Point(12, 12),
         iconSize: new L.Point(40, 40),
-        iconUrl: '/images/icon-camera.png'
+        iconUrl: './images/icon-camera.png'
       }
     });
 
@@ -503,39 +186,33 @@ export default {
       data_geoJson = req.data;
       //console.log(data_geoJson)
       var layerGeo = L.geoJSON(data_geoJson, {
-          style:myStyle
+        style:myStyle
       }).addTo(this.map);
       layerGeo.on('click',function(e){
-          console.log(e,e.layer.feature.properties.name) //当前点击的物体的名称
+        console.log(e,e.layer.feature.properties.name) //当前点击的物体的名称
       })
     })
 
     //轨迹图
-    var antLatlngs = [[233.4375,-562],[248.4375,-506],[237.4375,-457],[199.4375,-415],[145.4375,-384],[39.4375,-338],[-44.5625,-286],[-109.5625,-180],[-124.5625,-92],[-149.5625,-42],[-170.5625,-24]]
+    var antLatlngs = [[-12.890625,-348.5],[-7.640625,-336],[-7.640625,-321.5],[-12.140625,-309],[-21.140625,-299.5],[-39.640625,-287.5],[-64.890625,-278.5],[-95.140625,-261.75],[-114.640625,-243.25],[-123.140625,-226.25],[-129.390625,-204.25],[-133.390625,-186.5],[-139.140625,-174.25],[-144.390625,-167.75]]
     var antOptions = {delay: -300, dashArray: [10,20], weight: 10, color: "#FF00ea", pulseColor: "#FFFFFF"};
     
-    
-    
-    // this.map.on("zoomend", function(){
-    //   var zoomLeaval = this.map.getZoom();
+    this.map.on("zoomend", function(){
+      var zoomLeaval = this.map.getZoom();
+      console.log(zoomLeaval)
+      // antOptions.delay = 10+100*zoomLeaval
       
-    //   antOptions.delay = 10+100*zoomLeaval
-      
-      
-    //   var animationDuration = 1 + antOptions.delay/3/this.map.getZoom() +"s";
-    //   console.log(this.map.getZoom(),antOptions.delay,animationDuration);
-    //   let antPolyline = new AntPath(antLatlngs, antOptions);
-    // }, this);
+      // var animationDuration = 1 + antOptions.delay/3/this.map.getZoom() +"s";
+      // console.log(this.map.getZoom(),antOptions.delay,animationDuration);
+      // let antPolyline = new AntPath(antLatlngs, antOptions);
+    }, this);
     // Usethe constructor...
     let antPolyline = new AntPath(antLatlngs, antOptions);
-    
     // ... or use the factory
     //antPolyline = antPath(latlngs, options);
     
     antPolyline.addTo(this.map);
     
-   
-
   },
   methods: {
     zoomUpdate (zoom) {
